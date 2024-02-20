34fa1afd9e0d3f95d987305bc8c9ab4e6be3919a
Mohit Kukreja <113180620+MoHiTKuKrEjA11@users.noreply.github.com>
2024-02-20 22:50:02 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -18,7 +18,7 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-    .post('http://localhost:5555/books',data)
+    .post('https://backend-book-store-gs3k.onrender.com/books',data)
     .then(()=>{
       setLoading(false);
       navigate('/');
