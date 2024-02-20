18aca4a0100357489dc40a6f282dc9e588e08b09
MoHiTKuKrEjA11 <mohitkukreja463@gmail.com>
2024-02-20 15:24:43 +0530

    cors manipulation in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: 3000,
+		// Get rid of the CORS error
+		proxy: {
+			"/api": {
+				target: "https://backend-book-store-5drc.onrender.com",
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
+});
